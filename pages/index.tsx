b9d028ab95cbcea540ccc6503849d1f037709394
Vincent Battaglia <vincent.battaglia@gmail.com>
2022-09-16 10:52:31 +0200

    Logo + analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -9,6 +10,12 @@ const Home: NextPage = () => {
         <title>0x3 Studio API</title>
         <meta name="description" content="An API of web3-related stuff." />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          defer
+          data-website-id="a919dbee-fd49-4476-94d9-03ad964ab131"
+          src="https://umami.0x3.studio/umami.js"
+        ></script>
       </Head>
 
       <main className={styles.main}>
@@ -35,6 +42,17 @@ const Home: NextPage = () => {
           </li>
         </ul>
       </main>
+
+      <footer className={styles.footer}>
+        <a href="https://0x3.studio">
+          <Image
+            src="/logo-0x3-studio.svg"
+            width="134"
+            height="13"
+            alt="The beautiful and now famous logo of 0x3 Studio"
+          />
+        </a>
+      </footer>
     </div>
   );
 };
